1c4f2102d70debf9173ec3574e5a1d37d22fc249
Shane-Alternate <codyrikkuntheproto@gmail.com>
2025-06-20 14:38:45 -0400

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,15 @@
+// World Generation
+export const worldWidth = 64;
+export const worldDepth = 64;
+export const worldHeight = 32;
+export const noiseScale = 20;
+
+// Player
+export const playerSpeed = 5;
+export const playerJumpHeight = 8;
+export const playerWidth = 0.5;
+export const playerHeight = 1.8;
+export const gravity = -19.6; // (2 * 9.8)
+
+// Other
+export const selectedBlockType = 1; // Start with grass
